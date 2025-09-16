4303964b3a9f97e0f0688d0387b847bb7c768523
shubhamCda <shubhambodalkar281@gmail.com>
2025-09-16 23:03:03 +0530

    Add OTP reactjs app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Todo from './project/Todo'
 // import ProgressBar from './components/progress-bar/ProgressBar'
 import StarRating from './components/star-rating/StarRating'
+import OtpInput from './components/otp/OtpInput'
 
 function App() {
   return (
     <>
       {/* <Todo /> */}
       {/* <ProgressBar /> */}
-      <StarRating />
+      {/* <StarRating /> */}
+      <OtpInput />
     </>
   )
 }
--- src/components/otp/OtpInput.jsx
+++ src/components/otp/OtpInput.jsx
@@ -1,21 +1,52 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from "react";
 import "./otpInput.css";
 
 function OtpInput({ inputValue = 6 }) {
-  const [otpFields, setOtpFields] = useState(new Array(inputValue).fill(""));
-  return (
-    <>
-      <div className="container">
-        {
-          otpFields.map((curElement, index) => {
-            return (
-              <input type="number" key={index} />
-            )
-          })
-        }
-      </div>
-    </>
-  )
+	const [otpFields, setOtpFields] = useState(new Array(inputValue).fill(""));
+	const ref = useRef([]);
+
+	console.log(ref);
+
+	const handleKeyDown = (e, index) => {
+		let key = e.key;
+		console.log(key);
+
+		const copyOtpValues = [...otpFields];
+
+		if (key == "Backspace") {
+			copyOtpValues[index] = "";
+			setOtpFields(copyOtpValues);
+
+			if (0 <= index) ref.current[index - 1].focus();
+			return;
+		}
+		if (isNaN(key)) {
+			return;
+		}
+		copyOtpValues[index] = key;
+		if (index + 1 < otpFields.length) {
+			ref.current[index + 1].focus();
+		}
+		setOtpFields(copyOtpValues);
+		console.log(copyOtpValues);
+	};
+	return (
+		<>
+			<div className="container">
+				{otpFields.map((curElement, index) => {
+					return (
+						<input
+							type="text"
+							key={index}
+							ref={(curInput) => (ref.current[index] = curInput)}
+							value={curElement}
+							onKeyDown={(e) => handleKeyDown(e, index)}
+						/>
+					);
+				})}
+			</div>
+		</>
+	);
 }
 
-export default OtpInput
+export default OtpInput;
